173a20d8a6d3afcd629fc2e7c52e48f9a5dd361f
Daniel Mancuso <damancuso@gmail.com>
2015-03-23 00:24:04 +0100

    RTC event handler implemented for 1 timer

--- relayr/src/rtc.h
+++ relayr/src/rtc.h
@@ -7,7 +7,23 @@
 /**< Input frequency of the RTC instance. */
 #define RTC_INPUT_FREQ 32768
 
+#define RTC_MAX_TIMERS  4
 
-void rtc_init(void);
+struct rtc_ctx;
+
+typedef void (rtc_evt_cb_t)(struct rtc_ctx *ctx);
+
+struct rtc_x {
+  uint32_t period;  //24 bits, max value: 16777216 (~4 hours @ 1ms tick)
+  uint8_t enabled;
+  rtc_evt_cb_t *cb;
+};
+
+struct rtc_ctx {
+	uint8_t used_timers;  //Max 4
+  struct rtc_x rtc_x[RTC_MAX_TIMERS];
+};
+
+void rtc_init(struct rtc_ctx *ctx);
 
 #endif
