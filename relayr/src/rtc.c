6054ef74eb69e73733a01af9854023033c237437
Daniel Mancuso <damancuso@gmail.com>
2015-03-23 12:24:20 +0100

    RTC module working with up to 4 timers, handler callbacks based

--- relayr/src/rtc.c
+++ relayr/src/rtc.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 #include "simble.h"
 #include "rtc.h"
 
@@ -21,20 +23,40 @@ rtc_init(struct rtc_ctx *c)
 
   //The LFCLK runs at 32.768 Hz, divided by (PRESCALER+1) = 1.024 ms
   NRF_RTC1->PRESCALER = RTC_PRESCALER;
+  // Disable the Event routing to the PPI to save power
+  NRF_RTC1->EVTEN = 0;
 
-  NRF_RTC1->CC[0] = ctx->rtc_x[0].period;
+  for (int i=0; i < RTC_MAX_TIMERS; i++) {
+    if (ctx->used_timers < RTC_MAX_TIMERS && (ctx->rtc_x[i].period != 0)) {
+      NRF_RTC1->CC[i] = ctx->rtc_x[i].period;
+      ctx->used_timers++;
+    }
+  }
+
+  // Config. CC[x] module to generate interrupts and events
+  /* TODO: Nasty, find a better way to set the masks */
+  if (ctx->used_timers > 0){
+      NRF_RTC1->EVTENSET = RTC_EVTENSET_COMPARE0_Msk;
+      NRF_RTC1->INTENSET = RTC_INTENSET_COMPARE0_Msk;
+  }
+  if (ctx->used_timers > 1){
+      NRF_RTC1->EVTENSET = RTC_EVTENSET_COMPARE1_Msk;
+      NRF_RTC1->INTENSET = RTC_INTENSET_COMPARE1_Msk;
+  }
+  if (ctx->used_timers > 2){
+      NRF_RTC1->EVTENSET = RTC_EVTENSET_COMPARE2_Msk;
+      NRF_RTC1->INTENSET = RTC_INTENSET_COMPARE2_Msk;
+  }
+  if (ctx->used_timers > 3){
+      NRF_RTC1->EVTENSET = RTC_EVTENSET_COMPARE3_Msk;
+      NRF_RTC1->INTENSET = RTC_INTENSET_COMPARE3_Msk;
+  }
+
 
   // Reset the Counter
   NRF_RTC1->TASKS_CLEAR = 1;
   // Start the RTC1
   NRF_RTC1->TASKS_START = 1;
-
-  NRF_RTC1->EVTENSET = RTC_EVTENSET_COMPARE0_Msk;
-  // Config. CC[0] module to generate interrupts
-  NRF_RTC1->INTENSET = RTC_INTENSET_COMPARE0_Msk;
-
-  // Disable the Event routing to the PPI to save power
-  NRF_RTC1->EVTEN = 0;
 }
 
 void rtc_oneshot_timer(){
@@ -45,10 +67,8 @@ void rtc_oneshot_timer(){
 void
 RTC1_IRQHandler(void)
 {
-  NRF_GPIO->OUT ^= (1 << 1);
-
+  //NRF_GPIO->OUT ^= (1 << 1);
   for (uint8_t i = 0; i < ctx->used_timers; i++){
-
     if (NRF_RTC1->EVENTS_COMPARE[i] != 0)
     {
         // prepare the comparator for the next interval
@@ -60,7 +80,6 @@ RTC1_IRQHandler(void)
 
         //Toggle pin2 for test
         NRF_GPIO->OUT ^= (1 << 2);
-
         ctx->rtc_x[i].cb(ctx); //TODO:check this
         //sd_nvic_ClearPendingIRQ(RTC1_IRQn);
     }
