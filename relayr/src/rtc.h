2e7f2de35654fc5c9a57dc898f3fb0ede956e7fa
Daniel Tralamazza <daniel@tralamazza.com>
2015-04-04 04:01:39 +0200

    remove unused includes reindent code

--- relayr/src/rtc.h
+++ relayr/src/rtc.h
@@ -1,17 +1,17 @@
-#ifndef RTC_H
-#define RTC_H
+#pragma once
 
+#include <stdint.h>
 
 /**< Number of compare channels in the RTC1 instance. */
 #define RTC_CHANNEL_NUM 4
 /**< Input frequency of the RTC instance. */
 #define RTC_INPUT_FREQ 32768
 
-#define RTC_MAX_TIMERS  4
+#define RTC_MAX_TIMERS 4
 
-enum timer_type{
-    PERIODIC = 0,
-    ONE_SHOT = 1,
+enum timer_type {
+	PERIODIC,
+	ONE_SHOT,
 };
 
 struct rtc_ctx;
@@ -19,19 +19,17 @@ struct rtc_ctx;
 typedef void (rtc_evt_cb_t)(struct rtc_ctx *ctx);
 
 struct rtc_x {
-  uint8_t type;
-  uint32_t period;  //24 bits, max value: 16777216 (~4 hours @ 1ms tick)
-  bool enabled;
-  rtc_evt_cb_t *cb;
+	enum timer_type type;
+	uint32_t period;  //24 bits, max value: 16777216 (~4 hours @ 1ms tick)
+	bool enabled;
+	rtc_evt_cb_t *cb;
 };
 
 struct rtc_ctx {
-  uint8_t used_timers;  //Max 4
-  struct rtc_x rtc_x[RTC_MAX_TIMERS];
+	uint8_t used_timers;  //Max 4
+	struct rtc_x rtc_x[RTC_MAX_TIMERS];
 };
 
 void rtc_update_cfg(uint32_t time_ms, uint8_t timer_id, bool enabled);
 void rtc_init(struct rtc_ctx *ctx);
 bool rtc_oneshot_timer(uint32_t time_ms, rtc_evt_cb_t *cb);
-
-#endif
