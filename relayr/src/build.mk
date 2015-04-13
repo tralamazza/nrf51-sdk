aedfed995572786252225ae94732945811acf7b2
Daniel Tralamazza <daniel@tralamazza.com>
2015-04-13 17:32:45 +0200

    add support for device manager include RTT conditionally via "DEBUG_LOG= rtt"

--- relayr/src/build.mk
+++ relayr/src/build.mk
@@ -1,8 +1,7 @@
 RELAYR_ROOT?= ${SDKDIR}/relayr
 
 CFLAGS+= \
-	-I${RELAYR_ROOT}/src \
-	-I${SDKDIR}/segger/RTT
+	-I${RELAYR_ROOT}/src
 
 SDKINCDIRS+= \
 	device \
@@ -16,11 +15,23 @@ SRCS+= \
 	${RELAYR_ROOT}/src/simble.c \
 	${RELAYR_ROOT}/src/util.c \
 	${RELAYR_ROOT}/src/batt_serv.c \
-	${RELAYR_ROOT}/src/rtc.c \
+	${RELAYR_ROOT}/src/rtc.c
+
+ifeq (${DEBUG_LOG},rtt)
+CFLAGS+= \
+	-I${SDKDIR}/segger/RTT
+
+SRCS+= \
 	${RELAYR_ROOT}/src/segger_rtt_init.c \
 	${SDKDIR}/segger/RTT/SEGGER_RTT.c \
 	${SDKDIR}/segger/RTT/SEGGER_RTT_printf.c \
 	${SDKDIR}/segger/Syscalls/RTT_Syscalls_GCC.c
+
+DEFINES+= ENABLE_RTT_DEBUG_LOG
+endif
+
+SDKSRCS+= \
+	drivers_nrf/pstorage/pstorage.c
 
 ifeq (${USE_SOFTDEVICE},s120)
 DEFINES+= SD120
@@ -29,11 +40,11 @@ SRCS+= \
 	${RELAYR_ROOT}/src/simble_central.c
 
 SDKSRCS+= \
-	ble/device_manager/device_manager_central.c \
-	drivers_nrf/pstorage/pstorage.c
-
-SDKINCDIRS+= \
-	libraries/trace
+	ble/ble_db_discovery/ble_db_discovery.c \
+	ble/device_manager/device_manager_central.c
+else
+SDKSRCS+= \
+	ble/device_manager/device_manager_peripheral.c
 endif
 
 DEFINES+= BLE_STACK_SUPPORT_REQD SOFTDEVICE_PRESENT __HEAP_SIZE=0
